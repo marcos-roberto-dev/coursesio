be87bc9535d4f93b15641d154ba8346d8646abef
Marcos Roberto <marcosprojetsdev@gmail.com>
2024-12-07 22:45:52 -0300

    :art: update StudentEntity

--- src/main/java/br/com/couseio/courseio/modules/student/entities/StudentEntity.java
+++ src/main/java/br/com/couseio/courseio/modules/student/entities/StudentEntity.java
@@ -1,16 +1,21 @@
 package br.com.couseio.courseio.modules.student.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import br.com.couseio.courseio.modules.course.entities.CourseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -47,6 +52,10 @@ public class StudentEntity {
     @NotBlank
     @NotNull
     private String register;
+
+    @ManyToMany
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<CourseEntity> courses;
 
     @CreationTimestamp
     private LocalDateTime createdAt;

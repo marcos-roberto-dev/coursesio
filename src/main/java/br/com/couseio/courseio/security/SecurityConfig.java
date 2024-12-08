06eb57dbc070de734acc794709389ccb3a5419f0
Marcos Roberto <marcosprojetsdev@gmail.com>
2024-12-08 16:24:46 -0300

    :art: update SecurityConfig Cors

--- src/main/java/br/com/couseio/courseio/security/SecurityConfig.java
+++ src/main/java/br/com/couseio/courseio/security/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -16,10 +19,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorizeHttpRequestsCustomizer -> {
                     authorizeHttpRequestsCustomizer
-                            .requestMatchers("/api/v1/student/").permitAll()
-                            .requestMatchers("/api/v1/instructor/").permitAll();
+                            .requestMatchers("/api/v1/student/**").permitAll()
+                            .requestMatchers("/api/v1/instructor/**").permitAll()
+                            .requestMatchers("/api/v1/courses/**").permitAll() // Certifique-se de que esta linha está
+                                                                               // presente
+                            .requestMatchers("/swagger-ui/**").permitAll()
+                            .requestMatchers("/v3/api-docs/**").permitAll()
+                            .requestMatchers("/swagger-resources/**").permitAll()
+                            .requestMatchers("/webjars/**").permitAll()
+                            .requestMatchers("/api-docs/**").permitAll();
 
                     authorizeHttpRequestsCustomizer.anyRequest().authenticated();
                 });
@@ -27,7 +38,30 @@ public class SecurityConfig {
     }
 
     @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
